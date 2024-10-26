75dde3e3948da9f666e328b858d599d52a57bd67
parthsolanke <parthsolanke2002@gmail.com>
2024-10-27 00:26:09 +0530

    chore: add bubble sort

--- internal/sorter/sorter.go
+++ internal/sorter/sorter.go
@@ -4,18 +4,43 @@ type SortStep struct {
 	Array []int `json:"array"`
 }
 
+func SelectionSort(arr []int) []SortStep {
+	var steps []SortStep
+	n := len(arr)
+
+	for i := 0; i < n-1; i++ {
+		minIdx := i
+		for j := i + 1; j < n; j++ {
+			if arr[j] < arr[minIdx] {
+				minIdx = j
+			}
+		}
+		arr[i], arr[minIdx] = arr[minIdx], arr[i]
+
+		steps = appendStep(steps, arr)
+	}
+
+	return steps
+}
+
 func BubbleSort(arr []int) []SortStep {
 	var steps []SortStep
 	n := len(arr)
 
 	for i := 0; i < n-1; i++ {
+		swapped := false
 		for j := 0; j < n-i-1; j++ {
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
+				swapped = true
 			}
 		}
 
 		steps = appendStep(steps, arr)
+
+		if !swapped {
+			break
+		}
 	}
 
 	return steps
@@ -34,25 +59,6 @@ func InsertionSort(arr []int) []SortStep {
 			j = j - 1
 		}
 		arr[j+1] = key
-
-		steps = appendStep(steps, arr)
-	}
-
-	return steps
-}
-
-func SelectionSort(arr []int) []SortStep {
-	var steps []SortStep
-	n := len(arr)
-
-	for i := 0; i < n-1; i++ {
-		minIdx := i
-		for j := i + 1; j < n; j++ {
-			if arr[j] < arr[minIdx] {
-				minIdx = j
-			}
-		}
-		arr[i], arr[minIdx] = arr[minIdx], arr[i]
 
 		steps = appendStep(steps, arr)
 	}
